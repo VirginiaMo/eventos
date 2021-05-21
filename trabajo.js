d1adb614ec61daf9f76b0939e9cfc6c367480502
virginiamo <scandarelli@hotmail.com>
2021-05-21 15:01:55 +0200

    Primer ejercicio

---
+++ trabajo.js
@@ -0,0 +1,14 @@
+
+document.getElementById("modoLectura").onclick = function(){
+document.getElementById("container").style.background= "black";
+document.getElementById("container").style.color='#FFFFFF';
+document.getElementById("image").style="visibility:hidden";
+
+document.getElementById("modoNormal").onclick = function(){
+document.getElementById("container").style.background="white";
+document.getElementById("container").style.color= "black";
+document.getElementById("image").style="visibility";
+    
+}
+
+}
